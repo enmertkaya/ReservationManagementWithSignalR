1916b3c24f68a6197315eb9378d8fed15567075d
Enes Mert Kaya <merteneskaya@outlook.com>
2024-03-20 15:37:23 +0300

    kategori listesi api consume

--- SignalRQRReservationWebUI/Controllers/CategoryController.cs
+++ SignalRQRReservationWebUI/Controllers/CategoryController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace SignalRQRReservationWebUI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+        private readonly IHttpClientFactory _httpClientFactory;
+
+		public CategoryController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7018/api/Category");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<Dtos.CategoryDtos.ResultCategoryDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
+	}
 }
