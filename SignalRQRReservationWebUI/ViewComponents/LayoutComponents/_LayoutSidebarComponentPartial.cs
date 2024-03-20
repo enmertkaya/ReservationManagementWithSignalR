30a7b706143d590c942113fcd2b7f781e576f4da
Enes Mert Kaya <merteneskaya@outlook.com>
2024-03-20 14:14:18 +0300

    Layout Component Partial Eklendi (tema)

---
+++ SignalRQRReservationWebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRQRReservationWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutSidebarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
