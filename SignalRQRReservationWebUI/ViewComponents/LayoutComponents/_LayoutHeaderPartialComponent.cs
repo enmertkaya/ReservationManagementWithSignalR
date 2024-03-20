6ebd536dfaf5405a7c64d7cad6f4a2c14ec80383
Enes Mert Kaya <merteneskaya@outlook.com>
2024-03-20 13:49:35 +0300

    ViewComponent eklendi

---
+++ SignalRQRReservationWebUI/ViewComponents/LayoutComponents/_LayoutHeaderPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRQRReservationWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutHeaderPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
